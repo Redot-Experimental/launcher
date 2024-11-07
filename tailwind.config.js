9087a5fe951858f7913f90eadc2867838f9d182b
Charlotte <charlottewiltshire27@outlook.com>
2024-11-07 22:30:47 +0200

    fix: update route component to render JSX and configure Tailwind to support lazy-loaded components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/renderer/index.html', './src/renderer/src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/renderer/index.html', './src/renderer/src/**/*.{js,ts,jsx,tsx,lazy.tsx}'],
   theme: {
     extend: {}
   },
