19334e64ec4560b9e405a11310e0ae2493bb8112
Charlotte <charlottewiltshire27@outlook.com>
2024-11-09 14:17:32 +0200

    fix: replace Math.random with crypto.randomInt for secure random sidebar width

--- src/renderer/src/components/ui/sidebar.tsx
+++ src/renderer/src/components/ui/sidebar.tsx
@@ -11,6 +11,7 @@ import { Sheet, SheetContent } from '@/components/ui/sheet'
 import { Skeleton } from '@/components/ui/skeleton'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 import { cn } from '../../utils'
+import crypto from 'crypto'
 
 const SIDEBAR_COOKIE_NAME = 'sidebar:state'
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
@@ -630,7 +631,8 @@ const SidebarMenuSkeleton = React.forwardRef<
 >(({ className, showIcon = false, ...props }, ref) => {
   // Random width between 50 to 90%.
   const width = React.useMemo(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`
+    const randomWidth = 50 + crypto.randomInt(0, 41)
+    return `${randomWidth}%`
   }, [])
 
   return (
