a90d3fe5094761253fe9ac99abcc75b0fb687b62
Charlotte <charlottewiltshire27@outlook.com>
2024-11-09 15:32:31 +0200

    chore: remove unused imports in the new project route

--- src/renderer/src/routes/projects/new.tsx
+++ src/renderer/src/routes/projects/new.tsx
@@ -1,15 +1,5 @@
 import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
-import { SidebarInset, SidebarTrigger } from '../../components/ui/sidebar'
-import { Separator } from '../../components/ui/separator'
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from '../../components/ui/breadcrumb'
 import { SharedSidebarLayout } from '../../components/shared-sidebar-layout'
 
 export const Route = createFileRoute('/projects/new')({
