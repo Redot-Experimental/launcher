c4a5d3b3973fa6d619286c9fa99386308dc56fbb
Charlotte <charlottewiltshire27@outlook.com>
2024-11-09 13:09:50 +0200

    feat: add shared sidebar layout to /projects route

--- src/renderer/src/routes/projects/index.tsx
+++ src/renderer/src/routes/projects/index.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { SharedSidebarLayout } from '../../components/shared-sidebar-layout'
 
 export const Route = createFileRoute('/projects/')({
   component: RouteComponent
 })
 
-function RouteComponent(): string {
-  return 'Hello /projects/projects!'
+function RouteComponent() {
+  return (
+    <SharedSidebarLayout breadcrumbs={[{ href: '/projects/', label: 'Projects' }]} title="Overview">
+      asdasdas
+    </SharedSidebarLayout>
+  )
 }
