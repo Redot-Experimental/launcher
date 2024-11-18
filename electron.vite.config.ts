859a60844f1c8b292bec3ef2aa42b98d2a3ec61d
Charlotte <charlottewiltshire27@outlook.com>
2024-11-18 20:17:30 +0200

    chore: Refactor code for consistency and readability

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,32 +1,32 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
     resolve: {
       alias: {
-        '@main': resolve('src/main'),
-        '@resources': resolve('resources'),
-        '@shared': resolve('src/shared')
-      }
-    }
+        "@main": resolve("src/main"),
+        "@resources": resolve("resources"),
+        "@shared": resolve("src/shared"),
+      },
+    },
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
   },
   renderer: {
-    assetsInclude: 'src/renderer/assets/**',
+    assetsInclude: "src/renderer/assets/**",
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src'),
-        '@locales': resolve('src/locales'),
-        '@shared': resolve('src/shared')
-      }
+        "@renderer": resolve("src/renderer/src"),
+        "@locales": resolve("src/locales"),
+        "@shared": resolve("src/shared"),
+      },
     },
-    plugins: [react(), TanStackRouterVite(), ViteImageOptimizer()]
-  }
-})
+    plugins: [react(), TanStackRouterVite(), ViteImageOptimizer()],
+  },
+});
